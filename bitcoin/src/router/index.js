b6782c2a627a80fcfb6f36d64dbfca77fbca6ccf
Summer675 <masaiya1010@163.com>
2019-11-19 16:10:01 +0800

    新的vue项目

--- bitcoin/src/router/index.js
+++ bitcoin/src/router/index.js
@@ -1,12 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import Block from '@/components/Block'
 import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css'
-
+import 'element-ui/lib/theme-chalk/index.css';
 Vue.use(ElementUI);
-
 Vue.use(Router)
 
 export default new Router({
@@ -16,11 +13,6 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
-    },
-    {
-      path: '/block',
-      name: 'block',
-      component: Block
-    },
+    }
   ]
 })
